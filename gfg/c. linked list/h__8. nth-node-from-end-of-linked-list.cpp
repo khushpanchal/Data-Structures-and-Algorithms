abcb5317f0b301baf6c9e49665d0200f7eb294d9
khushpanchal <khush.panchal123@gmail.com>
2024-02-08 22:33:36 +0530

    linked list problems

---
+++ gfg/c. linked list/h__8. nth-node-from-end-of-linked-list.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+//Initial Template for C++
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+class Solution{
+public:
+//Q. https://www.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1
+    int getNthFromLast(Node *head, int n)
+    {
+           // Your code here
+        Node *temp = head;
+        
+        int count = 0;
+        while(temp!=NULL)
+        {
+            count++;
+            temp = temp->next;
+        }
+        
+        if(n > count)
+        {
+            return -1;
+        }
+        
+        int x = 0;
+        temp = head;
+        while((count-n) != x)
+        {
+            x++;
+            temp = temp ->next;
+        }
+        
+        return temp->data;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+    cout<<obj.getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
